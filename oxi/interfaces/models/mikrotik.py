544688dae1970b87591a002497c312b4b8f13531
IluaAir <ilya@air.ru>
2026-02-15 02:44:29 +0300

    Update .gitignore and refactor Mikrotik model to inherit from BaseDevice
    - Added *.txt files to .gitignore to exclude etc files.
    - Refactored Mikrotik class to inherit from BaseDevice for improved structure and functionality.

--- oxi/interfaces/models/mikrotik.py
+++ oxi/interfaces/models/mikrotik.py
@@ -1,5 +1,6 @@
 from oxi.interfaces import register_parser
+from oxi.interfaces.base import BaseDevice
 
 
 @register_parser(["routeros", "ros", "mikrotik"])
-class Mikrotik: ...
+class Mikrotik(BaseDevice): ...
