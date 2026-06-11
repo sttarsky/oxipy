c40cae1561d72d80ccc1bd925bbc7ae56b680e29
IluaAir <ilya@air.ru>
2026-06-11 23:22:45 +0300

    Update dependencies and clean up Huawei model code
    - Updated `pytest` to version 9.0.3 and `responses` to version 0.26.1 in `pyproject.toml`.
    - Removed the main execution block from the Huawei model for cleaner code structure.

--- oxi/interfaces/models/huawei.py
+++ oxi/interfaces/models/huawei.py
@@ -9,10 +9,3 @@ class Huawei(BaseDevice):
     def vlans(self) -> list[dict]:
         vlan_ids = self.raw.get("vlans", {}).get("vlan_ids", [])
         return [{"vlan_id": vlan} for vlan in vlan_ids]
-
-
-if __name__ == "__main__":
-    with open("./test4.txt") as file:
-        data = file.read()
-    huawei = Huawei(data)
-    print(huawei.parse())
